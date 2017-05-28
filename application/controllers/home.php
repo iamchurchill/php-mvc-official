6aab26b3aec93e0e2894df90b6781411a7bd8e65
iamfreddy <churchillmerediths@yahoo.com>
2017-05-28 21:40:49 +0000

    Remove space from home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,16 +1,12 @@
 <?php 
 	class Home extends Controller{
-		
 		public function index($id = 1, $name = 'Fred'){
 			$user = $this->model('User');
 			$user->id = $id;
 			$user->name = $name;
-			
 			$this->view('templates/head');
 			$this->view('templates/navbar');
-			
 			$this->view('home/index', ['name'=>$user->name, 'id'=>$user->id]);
-			
 			$this->view('templates/footer');
 		}
 	}
