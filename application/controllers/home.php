b5d5bd1ef95e575ea20d822cad57dd7343246290
iamfreddy <churchillmerediths@yahoo.com>
2017-05-28 22:15:14 +0000

    Rearranged content in home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,9 +4,12 @@
 			$user = $this->model('User');
 			$user->id = $id;
 			$user->name = $name;
+			
 			$this->view('templates/head');
 			$this->view('templates/navbar');
+			
 			$this->view('home/index', ['name'=>$user->name, 'id'=>$user->id]);
+			
 			$this->view('templates/footer');
 		}
 	}
